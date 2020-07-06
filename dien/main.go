10e55da6f917290eb89747d5aa356b78494d5388
chengduzhusiyu <chengduzhusiyu@proton.me>
2020-07-06 12:18:08 +0000

    Update app.scss

--- dien/main.go
+++ dien/main.go
@@ -33,3 +33,47 @@ func main() {
 	// Construct an in-memory graph from the serialized form.
 	graph := tf.NewGraph()
 	if err := graph.Import(model, ""); err != nil {
+		log.Fatal(err)
+	}
+
+	// Create a session for inference over graph.
+	session, err := tf.NewSession(graph, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer session.Close()
+
+	source := Dataprocess(16)
+	uids, mids, cats, mid_his, cat_his, mid_mask, sl := prepare_data(source)
+
+	// uids, mids, cats, mid_his, cat_his, mid_mask, length_x
+	inputOp0 := graph.Operation("Inputs/mid_his_batch_ph")
+	inputOp1 := graph.Operation("Inputs/cat_his_batch_ph")
+	inputOp2 := graph.Operation("Inputs/uid_batch_ph")
+	inputOp3 := graph.Operation("Inputs/mid_batch_ph")
+	inputOp4 := graph.Operation("Inputs/cat_batch_ph")
+	inputOp5 := graph.Operation("Inputs/mask")
+	inputOp6 := graph.Operation("Inputs/seq_len_ph")
+
+	// inputOp7 := graph.Operation("Inputs/noclk_mid_batch_ph")
+	// inputOp8 := graph.Operation("Inputs/noclk_cat_batch_ph")
+	// inputOp9 := graph.Operation("Inputs/target_ph")
+
+	o1 := graph.Operation("dien/fcn/Softmax")
+	output, err := session.Run(
+		map[tf.Output]*tf.Tensor{
+			inputOp0.Output(0): mid_his,
+			inputOp1.Output(0): cat_his,
+			inputOp2.Output(0): uids,
+			inputOp3.Output(0): mids,
+			inputOp4.Output(0): cats,
+			inputOp5.Output(0): mid_mask,
+			inputOp6.Output(0): sl,
+			// inputOp7.Output(0): noClkMidHis,
+			// inputOp8.Output(0): noClkCatHis,
+			// inputOp9.Output(0): target,
+		},
+		[]tf.Output{
+			o1.Output(0),
+		},
+		nil)
