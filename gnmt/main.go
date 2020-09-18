c3383875eb64b2fa55cd4b29547955c82b4d3ba9
chengduzhusiyu <chengduzhusiyu@proton.me>
2020-09-18 14:42:37 +0000

    README clarifications

--- gnmt/main.go
+++ gnmt/main.go
@@ -29,3 +29,43 @@ func main() {
 	C.free(unsafe.Pointer(pth))
 	stat := C.TF_NewStatus()
 	C.TF_LoadLibrary(pth, stat);
+	pp.Println( C.GoString(C.TF_Message(stat)))
+
+	model, err := tf.LoadSavedModel(dir, []string{"train", "serve"}, nil)
+	if err != nil {
+		pp.Printf("Error loading saved model: %s\n", err.Error())
+		return
+	}
+
+	defer model.Session.Close()
+
+	// // Input op
+	// inputOp := graph.Operation("input")
+
+	// // Output ops
+	// o1 := graph.Operation("MobilenetV1/Predictions/Reshape_1")
+
+	// // Execute COCO Graph
+	// output, err := model.session.Run(
+	// 	map[tf.Output]*tf.Tensor{
+	// 		inputOp.Output(0): tensor,
+	// 	},
+	// 	[]tf.Output{
+	// 		o1.Output(0),
+	// 	},
+	// 	nil)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// // Take the first in the batched output
+	// probabilities := output[0].Value().([][]float32)[0]
+
+	// idxs := make([]int, len(probabilities))
+	// for i := range probabilities {
+	// 	idxs[i] = i
+	// }
+	// preds := utils.Predictions{Probabilities: probabilities, Indexes: idxs}
+	// sort.Sort(preds)
+
+	// for ii := 0; ii < 1; ii++ {
+	// 	pp.Println(preds.Indexes[ii], labels[preds.Indexes[ii]], preds.Probabilities[ii])
