247b74d757ea1125a5e2853e8b8c56354b1ce0b9
chengduzhusiyu <chengduzhusiyu@proton.me>
2020-09-24 09:23:26 +0000

    " -> '

--- gnmt/main.py
+++ gnmt/main.py
@@ -3,4 +3,6 @@ import os
 import tensorflow as tf
 import tensorflow.contrib.resampler
 
-export_path = "/home/abduld/mlperf/inference/v0.5/trans
+export_path = "/home/abduld/mlperf/inference/v0.5/translation/gnmt/tensorflow/savedmodel"
+
+with t
