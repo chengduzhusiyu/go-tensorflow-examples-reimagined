3ec62e125963d02acdabcc6138e591f57d623553
chengduzhusiyu <chengduzhusiyu@proton.me>
2020-09-29 10:22:14 +0000

    update node modules

--- gnmt/main.py
+++ gnmt/main.py
@@ -6,4 +6,6 @@ import tensorflow.contrib.resampler
 export_path = "/home/abduld/mlperf/inference/v0.5/translation/gnmt/tensorflow/savedmodel"
 
 with tf.Session(graph=tf.Graph()) as sess:
-    tf.saved_model.loader.load(sess, ["train", "serve"], expor
+    tf.saved_model.loader.load(sess, ["train", "serve"], export_path)    
+    graph = tf.get_default_graph()
+  
