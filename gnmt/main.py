f16d7d89be3f2a786ce73209f3dc254de7a3d610
chengduzhusiyu <chengduzhusiyu@proton.me>
2020-09-29 07:49:38 +0000

    Typo in comments

--- gnmt/main.py
+++ gnmt/main.py
@@ -6,4 +6,4 @@ import tensorflow.contrib.resampler
 export_path = "/home/abduld/mlperf/inference/v0.5/translation/gnmt/tensorflow/savedmodel"
 
 with tf.Session(graph=tf.Graph()) as sess:
-    tf.save
+    tf.saved_model.loader.load(sess, ["train", "serve"], expor
