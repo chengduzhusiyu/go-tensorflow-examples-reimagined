a391e29368f54b1ae59cecb50f2ed6dd2b545ea6
chengduzhusiyu <chengduzhusiyu@proton.me>
2021-03-30 11:11:10 +0000

    minor edits

--- image_enhancement/freeze_model/download_data.sh
+++ image_enhancement/freeze_model/download_data.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-# The dataset is Validation Data Track 1 bicubic downscaling x4 (LR im
+# The dataset is Validation Data Track 1 bicubic downscaling x4 (LR images)	
+# from https://data.vision.ee.ethz.ch/cvl/DIV2K/	
+	
+w
