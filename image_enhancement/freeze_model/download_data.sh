8be49d6c6cea9f0be6583dd612dc14f4c3ab463c
chengduzhusiyu <chengduzhusiyu@proton.me>
2021-03-30 15:46:12 +0000

    Add gem

--- image_enhancement/freeze_model/download_data.sh
+++ image_enhancement/freeze_model/download_data.sh
@@ -3,4 +3,5 @@
 # The dataset is Validation Data Track 1 bicubic downscaling x4 (LR images)	
 # from https://data.vision.ee.ethz.ch/cvl/DIV2K/	
 	
-wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2
+wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_LR_bicubic_X4.zip	
+wget http://data.vision.ee
