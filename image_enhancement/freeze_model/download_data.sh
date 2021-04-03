bf65e5b03123fe47ef9b680c9eca186c5e803e3b
chengduzhusiyu <chengduzhusiyu@proton.me>
2021-04-03 07:06:27 +0000

    Share

--- image_enhancement/freeze_model/download_data.sh
+++ image_enhancement/freeze_model/download_data.sh
@@ -6,4 +6,5 @@
 wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_LR_bicubic_X4.zip	
 wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_HR.zip	
 	
-unzip DIV2
+unzip DIV2K_valid_LR_bicubic_X4.zip	
+unzip DIV2K_valid_H
