401ad76ac8c3de37a8c1dc706e8738ff6b4dd562
chengduzhusiyu <chengduzhusiyu@proton.me>
2021-04-03 05:51:17 +0000

    lab3

--- image_enhancement/freeze_model/download_data.sh
+++ image_enhancement/freeze_model/download_data.sh
@@ -4,4 +4,6 @@
 # from https://data.vision.ee.ethz.ch/cvl/DIV2K/	
 	
 wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_LR_bicubic_X4.zip	
-wget http://data.vision.ee
+wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_HR.zip	
+	
+unzip DIV2
