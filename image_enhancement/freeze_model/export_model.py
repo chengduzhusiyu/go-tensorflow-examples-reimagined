64e5921794ceeea8ad1ab6f7955bfde549af6552
chengduzhusiyu <chengduzhusiyu@proton.me>
2021-04-10 19:22:32 +0000

    update to 1.1.0

--- image_enhancement/freeze_model/export_model.py
+++ image_enhancement/freeze_model/export_model.py
@@ -7,4 +7,5 @@ from model import SRGAN_g
 
 # Uncomment the following line to print the GPU and tf and tl log
 # os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-# tf.logging.set_v
+# tf.logging.set_verbosity(tf.logging.DEBUG)
+# tl.logging.set_ve
