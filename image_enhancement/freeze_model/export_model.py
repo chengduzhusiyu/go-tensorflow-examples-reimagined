b6efc4316f9b49a9e849a1d89f64d18a78b525c7
chengduzhusiyu <chengduzhusiyu@proton.me>
2021-04-10 07:47:50 +0000

    'aaa'

--- image_enhancement/freeze_model/export_model.py
+++ image_enhancement/freeze_model/export_model.py
@@ -1,4 +1,8 @@
 import os
 import tensorflow as tf
 import tensorlayer as tl
-from tensorflow.python.tools import freeze
+from tensorflow.python.tools import freeze_graph
+from model import SRGAN_g
+
+
+# Uncomment the 
