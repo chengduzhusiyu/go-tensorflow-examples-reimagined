c8a5264779fbd16cd1bb67311d4254f3625d0e4d
chengduzhusiyu <chengduzhusiyu@proton.me>
2022-05-01 21:45:21 +0000

    update key

--- utils.go
+++ utils.go
@@ -12,3 +12,46 @@ import (
 	"bytes"
 	"fmt"
 	"image"
+	"image/color"
+	"image/png"
+	"io/ioutil"
+	"log"
+	"os"
+	"reflect"
+	"unsafe"
+
+	"github.com/disintegration/imaging"
+	imagetypes "github.com/rai-project/image/types"
+	tf "github.com/tensorflow/tensorflow/tensorflow/go"
+	"github.com/tensorflow/tensorflow/tensorflow/go/op"
+	"golang.org/x/image/colornames"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/basicfont"
+	"golang.org/x/image/math/fixed"
+)
+
+// DRAWING UTILITY FUNCTIONS
+
+// HLine draws a horizontal line
+func HLine(img *image.RGBA, x1, y, x2 int, col color.Color) {
+	for ; x1 <= x2; x1++ {
+		img.Set(x1, y, col)
+	}
+}
+
+// VLine draws a veritcal line
+func VLine(img *image.RGBA, x, y1, y2 int, col color.Color) {
+	for ; y1 <= y2; y1++ {
+		img.Set(x, y1, col)
+	}
+}
+
+// Rect draws a rectangle utilizing HLine() and VLine()
+func Rect(img *image.RGBA, x1, y1, x2, y2, width int, col color.Color) {
+	for i := 0; i < width; i++ {
+		HLine(img, x1, y1+i, x2, col)
+		HLine(img, x1, y2+i, x2, col)
+		VLine(img, x1+i, y1, y2, col)
+		VLine(img, x2+i, y1, y2, col)
+	}
+}
