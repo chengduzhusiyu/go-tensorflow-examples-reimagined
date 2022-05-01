e10ca538637ad298787d86b72861248345077c35
chengduzhusiyu <chengduzhusiyu@proton.me>
2022-05-01 11:14:22 +0000

    point

--- image_semantic_segmentation/main.go
+++ image_semantic_segmentation/main.go
@@ -47,3 +47,67 @@ func main() {
 	outjpg := flag.String("out", "output.jpg", "Path of output JPG for displaying labels. Default is output.jpg")
 	flag.Parse()
 	if *modeldir == "" || *jpgfile == "" {
+		flag.Usage()
+		return
+	}
+
+	// Load a frozen graph to use for queries
+	modelpath := filepath.Join(*modeldir, "frozen_inference_graph.pb")
+	model, err := ioutil.ReadFile(modelpath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Construct an in-memory graph from the serialized form.
+	graph := tf.NewGraph()
+	if err := graph.Import(model, ""); err != nil {
+		log.Fatal(err)
+	}
+
+	// Create a session for inference over graph.
+	session, err := tf.NewSession(graph, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer session.Close()
+
+	inputSize := 513
+	tensor, img, targetWidth, targetHeight, err := utils.MakeTensorFromResizedImage(*jpgfile, int32(inputSize))
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Input op
+	inputOp := graph.Operation("ImageTensor")
+
+	// Output ops
+	outputOp := graph.Operation("SemanticPredictions")
+
+	// Execute COCO Graph
+	output, err := session.Run(
+		map[tf.Output]*tf.Tensor{
+			inputOp.Output(0): tensor,
+		},
+		[]tf.Output{
+			outputOp.Output(0),
+		},
+		nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Take the first in the batched output
+	seg := output[0].Value().([][][]int64)[0]
+
+	colorMap := createPascalLabelColorMap()
+	imgSeg := image.NewRGBA(image.Rect(0, 0, targetWidth, targetHeight))
+	for w := 0; w < targetWidth; w++ {
+		for h := 0; h < targetHeight; h++ {
+			v := seg[h][w]
+			if v != 0 {
+				R, G, B := uint8(colorMap[v][0]), uint8(colorMap[v][1]), uint8(colorMap[v][2])
+				imgSeg.Set(w, h, color.RGBA{R, G, B, 255})
+			}
+		}
+	}
+
+	imgResized := imaging.Resize(img, targetWidth, targetHeight, imaging.Linear)
